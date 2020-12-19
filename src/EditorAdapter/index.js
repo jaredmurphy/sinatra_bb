08c1f5511c87587949a27e1a723ead91f3b4b008
Jared Murphy <jaredcmurphy@gmail.com>
2020-12-19 15:24:04 -0500

    ad alignment

--- src/EditorAdapter/index.js
+++ src/EditorAdapter/index.js
@@ -1,19 +1,25 @@
 import Quill from 'quill';
+import ImageResize from "quill-image-resize-module--fix-imports-error";
 
 class EditorAdapter {
   constructor() {
     this.toolbarOptions = [
-      ['bold', 'italic', 'strike'],
       [{ 'header': 1 }, { 'header': 2 }],
+      ['bold', 'italic', 'strike'],
       ['link', 'image'],
       [{ 'indent': '-1'}, { 'indent': '+1' }],
+      [{ 'align': 'start'}, { 'align': 'center' }],
+      //[{ 'align': '' }, { 'align': 'center' }, { 'align': 'right' }, { 'align': 'justify' }]
       ['clean'],
     ];
+
+    Quill.register("modules/imageResize", ImageResize);
 
     this.editor = new Quill('#editor', {
       theme: 'snow',
       modules: {
         toolbar: this.toolbarOptions,
+        imageResize: {},
       },
     });
 
