ea841b32b3d13fbbdb5ee48da85a26a15500470e
Jared Murphy <jaredcmurphy@gmail.com>
2020-12-19 22:13:33 -0500

    add basic handrolled editor with footnote links

--- src/EditorAdapter/index.js
+++ src/EditorAdapter/index.js
@@ -1,28 +1,29 @@
-import Quill from 'quill';
-import ImageResize from "quill-image-resize-module--fix-imports-error";
+//import Quill from 'quill';
+//import ImageResize from "quill-image-resize-module--fix-imports-error";
 
 class EditorAdapter {
   constructor() {
-    this.toolbarOptions = [
-      [{ 'header': 1 }, { 'header': 2 }],
-      ['bold', 'italic', 'strike'],
-      ['link', 'image'],
-      [{ 'indent': '-1'}, { 'indent': '+1' }],
-      [{ 'align': 'start'}, { 'align': 'center' }],
-      //[{ 'align': '' }, { 'align': 'center' }, { 'align': 'right' }, { 'align': 'justify' }]
-      ['clean'],
-    ];
+    //this.toolbarOptions = [
+      //[{ 'header': 1 }, { 'header': 2 }],
+      //['bold', 'italic', 'strike'],
+      //['link', 'image'],
+      //[{ 'indent': '-1'}, { 'indent': '+1' }],
+      //[{ 'align': 'start'}, { 'align': 'center' }],
+      ////[{ 'align': '' }, { 'align': 'center' }, { 'align': 'right' }, { 'align': 'justify' }]
+      //['clean'],
+    //];
 
-    Quill.register("modules/imageResize", ImageResize);
+    //Quill.register("modules/imageResize", ImageResize);
 
-    this.editor = new Quill('#editor', {
-      theme: 'snow',
-      modules: {
-        toolbar: this.toolbarOptions,
-        imageResize: {},
-      },
-    });
+    //this.editor = new Quill('#editor', {
+      //theme: 'snow',
+      //modules: {
+        //toolbar: this.toolbarOptions,
+        //imageResize: {},
+      //},
+    //});
 
+    const editor = document.getElementById('richTextEditor');
     const hiddenInput = document.getElementById('hidden-post-body-input');
     const postForm = document.getElementById('post-form');
 
