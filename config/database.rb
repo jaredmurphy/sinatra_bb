afae897719fc57baee2fefa8fa3171a073f009fb
Jared Murphy <jaredcmurphy@gmail.com>
2020-12-23 16:17:40 -0500

    database url

--- config/database.rb
+++ config/database.rb
@@ -2,6 +2,10 @@
 
 # set the database based on the current environment
 database_name = "brassbull_#{BrassBull.environment}"
+puts "-----------------------------------"
+puts "DATBASE URL"
+puts ENV["DATABASE_URL"]
+puts "-----------------------------------"
 db = URI.parse(ENV["DATABASE_URL"] || "postgres://localhost/#{database_name}")
 
 # connect ActiveRecord with the current database
